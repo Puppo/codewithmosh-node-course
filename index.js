eb7ee96714dcc7de074c6abf86938d018a4ee0af
Luca Del Puppo <luca@delpuppo.net>
2020-03-22 15:47:27 +0100

    add configuration

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
 const express = require('express');
 const app = express();
 
 const genresRoute = require('./genres');
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`app: env: ${app.get('env')}`);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'))
+
+// Configuration
+console.log(`Application Name: ${config.get('name')}`);
+console.log(`Application Mail Host: ${config.get('mail').host}`);
+console.log(`Application Mail Password: ${config.get('mail').password}`);
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
+
 
 app.get('/', (req, res) => {
     res.send('Hello To Vidly');
